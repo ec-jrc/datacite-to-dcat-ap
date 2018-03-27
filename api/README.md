7e7b281eee34e08901f7f611e8074bd375ecba33
peregan <andrea.perego@ec.europa.eu>
2018-03-28 00:41:22 +0200

    Initial commit.

---
+++ api/README.md
@@ -0,0 +1,144 @@
+﻿
+# The DataCite+DCAT-AP API (DataCite+DCAT-API)
+
+This API is a proof-of-concept of the implementation of DataCite+DCAT-AP in an [OAI-PMH endpoint](https://www.openarchives.org/pmh/), re-using the standard OAI-PMH interface, and supporting in addition [HTTP content negotiation](https://tools.ietf.org/html/rfc7231#section-3.4).
+
+More precisely, DataCite+DCAT-API uses the standard OAI-PMH parameter `metadataPrefix` to determine the DataCite+DCAT-AP profile to be used (core or extended).
+
+The document containing the DataCite records to be transformed is specified by a DataCite+DCAT-API-specific parameter `src`, whereas parameter `outputFormat` specifies the RDF serialisation to be returned. Both these parameter are not part of the OAI-PMH interface. 
+
+The API uses the [DataCite+DCAT-AP XSLT](https://webgate.ec.europa.eu/CITnet/stash/projects/ODCKAN/repos/iso-19139-to-dcat-ap) to transform DataCite records into DataCite+DCAT-AP. As such, the API works both on static files including the records, and on the OAI-PMH output of a `GetRecords` or `GetRecordById` request.
+
+# API specification
+
+## Supported HTTP methods
+
+The current version of DataCite+DCAT-API supports only the HTTP `GET` method. As a consequence, it can be used only on OAI-PMH endpoints supporting `GET` requests.
+
+## API parameters
+
+### Request
+
+<table style="width:100%;">
+  <thead>
+    <tr>
+      <th>Parameter</th>
+      <th>Description</th>
+      <th colspan="2">Possible values</th>
+      <th>Default value</th>
+      <th>Notes</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td rowspan="2"><code>metadataPrefix</code></td>
+      <td rowspan="2">The DataCite+DCAT-AP profile to be used for the transformation</td>
+      <td><code>core</code></td><td>(DCAT-AP)</td>
+      <td rowspan="2"><code>core</code></td>
+      <td rowspan="2">
+        <p>If this parameter is omitted, the API uses the "core" profile as default.</p>
+        <p>The "core" profile is labelled "DCAT-AP", since it returns just the metadata elements supported in DCAT-AP.</p>
+        <p><strong>NB</strong>: The current values of this parameter are provisional, and they are meant to be replaced by the official namespace URIs of DCAT-AP and DataCite+DCAT-AP, when available.</p>
+      </td>
+    </tr>
+    <tr>
+      <td><code>extended</code></td><td>(DataCite+DCAT-AP)</td>
+    </tr>
+    <tr>
+      <td rowspan="6"><code>outputFormat</code></td>
+      <td rowspan="6">The RDF serialisation to be returned</td>
+      <td><code>application/rdf+xml</code></td><td>(<a href="http://www.w3.org/TR/rdf-syntax-grammar/">RDF/XML</a>)</td>
+      <td rowspan="6">N/A</td>
+      <td rowspan="6">If this parameter is omitted, the returned RDF serialisation is determined via HTTP content negotiation</td>
+    </tr>
+    <tr>
+      <td><code>text/turtle</code></td><td>(<a href="http://www.w3.org/TR/turtle/">Turtle</a>)</td>
+    </tr>
+    <tr>
+      <td><code>text/n3</code></td><td>(<a href="http://www.w3.org/TeamSubmission/n3/">Notation 3</a>)</td>
+    </tr>
+    <tr>
+      <td><code>application/n-triples</code></td><td>(<a href="http://www.w3.org/TR/n-triples/">N-Triples</a>)</td>
+    </tr>
+    <tr>
+      <td><code>application/ld+json</code></td><td>(<a href="http://www.w3.org/TR/json-ld/">JSON-LD</a>)</td>
+    </tr>
+    <tr>
+      <td><code>text/html</code></td><td>(<a href="http://www.w3.org/TR/html-rdfa/">HTML+RDFa</a>)</td>
+    </tr>
+    <tr>
+      <td><code>src</code></td>
+      <td>The URL of the resource containing the DataCite records to be tranformed</td>
+      <td colspan="2">A URL</td>
+      <td>N/A</td>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
+
+### Response
+
+Besides the resulting RDF serialisation of the source DataCite records, the API returns a set of HTTP [`Link`](https://tools.ietf.org/html/rfc5988) headers, and the corresponding HTML LINK elements in the HTML+RDFa serialisation.
+
+<table width="100%">
+  <thead>
+    <tr>
+      <th><a href="http://www.iana.org/assignments/link-relations/" title="IANA Link Relations">Relation type</a></th>
+      <th>Type</th>
+      <th>Title</th>
+      <th>Target URI</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><code>derivedfrom</code></td>
+      <td><code>application/xml</code></td>
+      <td><code>DataCite</code></td>
+      <td>The URL of the source document, containing the DataCite records.</td>
+    </tr>
+    <tr>
+      <td rowspan="2"><code>profile</code></td>
+      <td rowspan="2">The media type of the document returned by the API.</td>
+      <td><code>DCAT-AP</code></td>
+      <td><code>core</code></td>
+    </tr>
+    <tr>
+      <td><code>DataCite+DCAT-AP</code></td>
+      <td><code>extended</code></td>
+    </tr>
+    <tr>
+      <td><code>self</code></td>
+      <td>The media type of the document returned by the API.</td>
+      <td>The name of the returned RDF serialisation.</td>
+      <td>The URL of the document returned by the API.</td>
+    </tr>
+    <tr>
+      <td><code>alternate</code></td>
+      <td>The media types of the alternative RDF serialisations supported by the API.</td>
+      <td>The name of the relevant RDF serialisation.</td>
+      <td>The URL of the document, encoded with the relevant RDF serialisation, as would be returned by the API.</td>
+    </tr>
+  </tbody>
+</table>
+
+# Implementation details
+
+DataCite+DCAT-API is implemented in [PHP5](http://php.net/), and runs on top of an [Apache 2 HTTP server](http://httpd.apache.org/).
+
+The [EasyRDF](http://www.easyrdf.org/) and the [ML/JSON-LD](https://github.com/lanthaler/JsonLD) PHP libraries are used to generate the supported RDF serialisations. The HTML+RDFa serialisation is generated by using the [DCAT-AP in HTML+RDFa](../../../dcat-ap-rdf2html/) XSLT.
+
+# Installation instructions
+
+DataCite+DCAT-API has been tested on both Linux and Windows, with Apache 2 and PHP 5.3.2 (or later) installed and running.
+
+**NB**: DataCite+DCAT-API makes use of the [PHP XSL extension](http://php.net/manual/en/xsl.installation.php).
+
+The repository includes all what is necessary, with the exception of EasyRDF and ML/JSON-LD, that must be installed separately by using [Composer](https://getcomposer.org/).
+
+More precisely:
+
+* Go to folder [`./lib/composer/`](./lib/composer/).
+* [Download Composer](https://getcomposer.org/download/). E.g.: `curl -s https://getcomposer.org/installer | php`
+* Run `php composer.phar install`
+
+You will now be able to run the API from a Web folder.
---
+++ api/index.php
@@ -0,0 +1,311 @@
+<?php
+
+// The API
+
+  $apiSrcRep = "https://webgate.ec.europa.eu/CITnet/stash/projects/ODCKAN/repos/datacite-to-dcat-ap/browse/api";
+
+// Variables for API landing page.
+
+  $title = "DataCite+DCAT-AP API";
+  $head = '<link rel="stylesheet" type="text/css" href="./css/style.css"/>' . "\n";
+  $footer = '<p>' . $title . ' @ Stash: <a href="' . $apiSrcRep . '">' . $apiSrcRep . '</a></p>';
+  $exampleSrcURL = "";
+
+// Loading the required libraries
+
+  require('./lib/composer/vendor/autoload.php');
+
+// Output schemas
+
+  $outputSchemas = array();
+  $outputSchemas['core'] = array(
+    'label' => 'DCAT-AP',
+    'description' => 'TBD',
+    'url' => 'TBD',
+    'xslt' => 'https://webgate.ec.europa.eu/CITnet/stash/projects/ODCKAN/repos/datacite-to-dcat-ap/browse/datacite-to-dcat-ap.xsl?raw',
+    'params' => array(
+      'profile' => 'core'
+    )
+  );
+  $outputSchemas['extended'] = array(
+    'label' => 'DataCite+DCAT-AP',
+    'description' => 'TBD',
+    'url' => 'TBD',
+    'xslt' => 'https://webgate.ec.europa.eu/CITnet/stash/projects/ODCKAN/repos/datacite-to-dcat-ap/browse/datacite-to-dcat-ap.xsl?raw',
+    'params' => array(
+      'profile' => 'extended'
+    )
+  );
+  $defaultOutputSchema = 'core';
+
+// XSLT to generate the HTML+RDFa serialisation
+
+  $rdf2rdfa = "https://webgate.ec.europa.eu/CITnet/stash/projects/ODCKAN/repos/dcat-ap-rdf2html/browse/dcat-ap-rdf2rdfa.xsl?raw";
+
+// Output formats
+
+  $outputFormats = array();
+  $outputFormats['application/rdf+xml'] = array('RDF/XML','rdf');
+  $outputFormats['text/turtle'] = array('Turtle','turtle');
+  $outputFormats['text/n3'] = array('N3','n3');
+  $outputFormats['application/n-triples'] = array('N-Triples','ntriples');
+  $outputFormats['application/ld+json'] = array('JSON-LD','jsonld');
+  $outputFormats['text/html'] = array('HTML+RDFa','');
+  $defaultOutputFormat = 'application/rdf+xml';
+
+// HTTP codes & corresponding pages
+
+  function returnHttpError($code) {
+    global $head;
+    $title =  $_SERVER["SERVER_PROTOCOL"] . ' ' . $code;
+    $content = '';
+    switch ($code) {
+      case 300:
+        $title .= ' Multiple Choices';
+        $content = '';
+        break;
+      case 404:
+        $title .= ' Not Found';
+        $content = 'The requested URL <code>' . $_SERVER["REQUEST_URI"] . '</code> was not found on this server.';
+        break;
+      case 415:
+        $title .= ' Unsupported Media Type';
+        $content = 'The server does not support the media type transmitted in the request.';
+        break;
+    }
+    http_response_code($code);
+    echo '<!DOCTYPE html><html><head><title>' . $title . '</title>' . str_replace("\n", "", $head) . '</head><body><header><h1>' . $title . '</h1></header><section><p>' . $content . '</p></section></body></html>';
+    exit;
+  }
+
+// Setting the output schema
+
+  if (isset($_GET['src'])) {
+
+    $xmluri = $_GET['src'];
+
+    $outputSchema = $defaultOutputSchema;
+    $xsluri = $outputSchemas[$outputSchema]['xslt'];
+    if (isset($_GET['metadataPrefix'])) {
+      if (isset($outputSchemas[$_GET['metadataPrefix']])) {
+        $outputSchema = $_GET['metadataPrefix'];
+        $xsluri = $outputSchemas[$outputSchema]['xslt'];
+      }
+      else {
+        returnHttpError(404);
+      }
+    }
+
+// Loading the source document 
+
+    $xml = new DOMDocument;
+    if (!$xml->load($xmluri)) {
+      returnHttpError(404);
+    }
+
+// Loading the XSLT to transform the source document into RDF/XML
+
+    $xsl = new DOMDocument;
+    if (!$xsl->load($xsluri)) {
+      returnHttpError(404);
+    }
+
+// Transforming the source document into RDF/XML
+
+    $proc = new XSLTProcessor();
+    $proc->importStyleSheet($xsl);
+
+    foreach ($outputSchemas[$outputSchema]['params'] as $k => $v) {
+      $proc->setParameter("", $k, $v);
+    }
+
+    if (!$rdf = $proc->transformToXML($xml)) {
+      returnHttpError(404);
+    }
+
+// Setting the output format
+
+    $outputFormat = $defaultOutputFormat;
+    if (isset($_GET['outputFormat'])) {
+      if (!isset($outputFormats[$_GET['outputFormat']])) {
+        returnHttpError(415);
+      }
+      else {
+        $outputFormat = $_GET['outputFormat'];
+      }
+    }
+    else {
+      preg_match_all("/[a-zA-Z0-9]+\/[a-zA-Z0-9\!\#\$\&\-\^_\.\+]+/",$_SERVER['HTTP_ACCEPT'],$matches);
+      if (count($matches[0]) == 0) {
+        returnHttpError(415);
+      }
+      else {
+        $acceptedFormats = $matches[0];
+        $supportedFormats = array_keys($outputFormats);
+        $candidateFormats = array_values(array_intersect($supportedFormats, $acceptedFormats));
+        switch (count($candidateFormats)) {
+          case 0:
+            returnHttpError(415);
+            break;
+          case 1:
+            $outputFormat = $candidateFormats[0];
+            break;
+          default:
+            returnHttpError(300);
+            break;
+        }
+      }
+    }
+
+// Related resources
+
+// The metadata profile of the output resource (output schema)
+    $link[] = array(
+      "href" => $outputSchema,
+      "rel" => "profile",
+      "type" => $outputFormat,
+      "title" => $outputSchemas[$outputSchema]["label"]
+    );
+// The input resource
+    $link[] = array(
+      "href" => $xmluri,
+      "rel" => "derivedfrom",
+      "type" => "application/xml",
+      "title" => "ISO 19139"
+    );
+// The available serialisations of the output resource (output format)
+    foreach ($outputFormats as $k => $v) {
+      $uri = str_replace($_SERVER['QUERY_STRING'], '', $_SERVER['REQUEST_URI']) . 'metadataPrefix=' . rawurlencode($outputSchema) . '&src=' . rawurlencode($xmluri) . '&outputFormat=' . rawurlencode($k);
+      $rel = 'alternate';
+      if ($k == $outputFormat) {
+        $rel = 'self';
+      }
+      $link[] = array(
+        "href" => $uri,
+        "rel" => $rel,
+        "type" => $k,
+        "title" => $v[0]
+      );
+      $outputFormats[$k][] = $uri;
+    }
+
+// Building HTTP "Link" headers and HTML "link" elements pointing to the related resources
+
+    $linkHTTP = array();
+    $linkHTML = array();
+    foreach ($link as $v) {
+      $linkHTTP[] = '<' . $v["href"] . '>; rel="' . $v["rel"] . '"; type="' . $v["type"] . '"; title="' . $v["title"] . '"';
+      $linkHTML[] = '<link href="' . $v["href"] . '" rel="' . $v["rel"] . '" type="' . $v["type"] . '" title="' . $v["title"] . '"/>';
+    }
+
+// Setting namespace prefixes
+
+    EasyRdf_Namespace::set('adms', 'http://www.w3.org/ns/adms#');
+    EasyRdf_Namespace::set('cnt', 'http://www.w3.org/2011/content#');
+    EasyRdf_Namespace::set('dc', 'http://purl.org/dc/elements/1.1/');
+    EasyRdf_Namespace::set('dcat', 'http://www.w3.org/ns/dcat#');
+    EasyRdf_Namespace::set('gsp', 'http://www.opengis.net/ont/geosparql#');
+    EasyRdf_Namespace::set('locn', 'http://www.w3.org/ns/locn#');
+    EasyRdf_Namespace::set('prov', 'http://www.w3.org/ns/prov#');
+
+// Creating the RDF graph from the RDF/XML serialisation
+
+    $graph = new EasyRdf_Graph;
+    $graph->parse($rdf);
+
+// Sending HTTP headers
+
+    header("Content-type: " . $outputFormat . ";charset=UTF8");
+    header('Link: ' . join(', ', $linkHTTP));
+
+// Returning the resulting document
+
+    if ($outputFormat == 'text/html') {
+      $xml = new DOMDocument;
+// From the raw RDF/XML output of the XSLT
+      $xml->loadXML($rdf) or die();
+// From the re-serialised RDF/XML output of the XSLT
+//      $xml->loadXML($graph->serialise("rdfxml")) or die();
+      $xsl = new DOMDocument;
+      $xsl->load($rdf2rdfa);
+      $proc = new XSLTProcessor();
+      $proc->importStyleSheet($xsl);
+// The title of the HTML+RDFa
+      $proc->setParameter('', 'title', $title);
+// The URL of the repository
+      $proc->setParameter('', 'home', $apiSrcRep);
+// All what needs to be added in the HEAD of the HTML+RDFa document
+      $head .= join("\n", $linkHTML) . "\n";
+      $proc->setParameter('', 'head', $head);
+      echo $proc->transformToXML($xml);
+      exit;
+    }
+    else {
+// Block added to enable pretty-print output of the JSON-LD serialisation, not supported in the current version of EasyRdf
+// Predefined constants are as per ML/JsonLD - see: https://github.com/lanthaler/JsonLD/blob/master/JsonLD.php
+      if ($outputFormat == 'application/ld+json') {
+        echo json_encode(json_decode($graph->serialise($outputFormats[$outputFormat][1])), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+      }
+      else {
+        echo $graph->serialise($outputFormats[$outputFormat][1]);
+      }
+// To be used when JSON-LD pretty-print will be supported in EasyRdf (see previous comment)      
+//      echo $graph->serialise($outputFormats[$outputFormat][1]);
+      exit;
+    }
+
+  }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title><?php echo $title; ?></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<?php echo $head; ?>
+  </head>
+  <body>
+    <header><h1><?php echo $title; ?></h1></header>
+    <nav>
+    </nav>
+    <section id="input-box">
+      <form id="api" action="." method="get">
+        <h1>
+          <label for="metadataPrefix">Output Schema : </label>
+          <select id="metadataPrefix" name="metadataPrefix">
+<?php foreach ($outputSchemas as $k => $v) { echo '            <option value="' . $k . '">' . $v['label'] . '</option>' . "\n"; } ?>
+          </select>
+          <input style="float:right" type="submit" id="transform" value="Transform"/>
+        </h1>
+        <p><input type="url" required="required" autofocus="autofocus" id="src" name="src" title="Copy &amp; paste the URL of DataCite records" placeholder="Copy &amp; paste the URL of DataCite records" value="<?php echo $exampleSrcURL?>"/></p>
+        <p style="float:right;">
+          <label for="outputFormat">Output format : </label>
+          <select id="outputFormat" name="outputFormat">
+<?php
+
+  foreach ($outputFormats as $k => $v) {
+    $selected = '';
+    if ($k == $defaultOutputFormat) {
+      $selected = ' selected="selected"';
+    }
+    echo '            <option value="' . $k . '"' . $selected . '>' . $v[0] . '</option>' . "\n";
+  }
+
+?>
+          </select>
+        </p>
+      </form>
+    </section>
+    <section>
+      <h1>Usage notes</h1>
+      <p>Copy &amp; paste the URL of a file or of an OAI-PMH request returning DataCite records.</p>
+      <p>Supported OAI-PMH request types: <code>ListRecords</code>, <code>GetRecord</code>.</p>
+      <p>Supported OAI-PMH output schema (<code>metadataPrefix</code>): <code>oai_datacite</code></p>
+      <p><strong>NB</strong>: The current version of the API supports only OAI-PMH calls using the <code>GET</code> HTTP method.</p>
+      <p><em>A description of the DataCite+DCAT-AP API is available on the <a href="<?php echo $apiSrcRep; ?>">API's Stash repository</a>.</em></p>
+    </section>
+    <aside>
+    </aside>
+    <footer><?php echo $footer; ?></footer>
+  </body>
+</html>
