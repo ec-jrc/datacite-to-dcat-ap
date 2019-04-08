af105ae01dc5993db9189766f952717ea5f3b7b7
Andrea <ndr.prg@gmail.com>
2019-04-08 23:29:26 +0200

    Switched to the standard respec-w3c-common build. Added ORCIDs.

--- docs/config.js
+++ docs/config.js
@@ -1,8 +1,4 @@
 var respecConfig = {
-// ISA specific
-//    version: "1.0.2",
-    versionURL: "https://ec-jrc.github.io/datacite-to-dcat-ap/",
-    latestVersionURL: "https://ec-jrc.github.io/datacite-to-dcat-ap/",
 
 //	preProcess: [dfn_index],
 //    logos: [{
@@ -46,9 +42,11 @@ var respecConfig = {
     edDraftURI:           "https://ec-jrc.github.io/datacite-to-dcat-ap/",
     issueBase:            "https://github.com/ec-jrc/datacite-to-dcat-ap/issues/",
     github:               "https://github.com/ec-jrc/datacite-to-dcat-ap/",
+    overrideCopyright: '<p class="copyright"><a href="https://europa.eu/european-union/abouteuropa/legal_notices_en#copyright_notice">Copyright</a> © 2015-2019 <a href="https://europa.eu/">European Union</a>. This document is licensed under a <a rel="license" href="https://creativecommons.org/licenses/by/4.0/" class="subfoot">Creative Commons Attribution 4.0 License</a>.</p>',
     editors: [
     {
       name:       "Andrea Perego",
+      orcid:      "0000-0001-9300-2694",
       company:    "European Commission, Joint Research Centre",
       url: "",
       companyURL: "https://ec.europa.eu/jrc/"
@@ -63,6 +61,7 @@ var respecConfig = {
     authors: [
     {
       name:       "Andrea Perego",
+      orcid:      "0000-0001-9300-2694",
       company:    "European Commission, Joint Research Centre",
       url: "",
       companyURL: "https://ec.europa.eu/jrc/"
@@ -112,7 +111,7 @@ var respecConfig = {
 //    wgPublicList: "",
 //    wgPatentURI: "",
     inlineCSS: "true",
-	  lint: "false",
+    lint: "false",
     localBiblio: {
       "DataCite2RDF": {
         "href":"https://doi.org/10.6084/m9.figshare.2075356",
